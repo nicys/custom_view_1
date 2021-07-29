7061185178268ded526821b1b921843c5cbb14b8
nicys <nicys2476@yandex.ru>
2021-07-29 21:46:29 +0700

    added

---
+++ app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
@@ -0,0 +1,151 @@
+package ru.netology.nmedia.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.withStyledAttributes
+import ru.netology.nmedia.R
+import ru.netology.nmedia.util.AndroidUtils
+import kotlin.math.min
+import kotlin.random.Random
+
+
+const val NUMBER_OF_DIVISIONS = 4F
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+    private var radius = 0F
+    private var center = PointF(0F, 0F)
+    private var oval = RectF(0F, 0F, 0F, 0F)
+
+    private var lineWidth = AndroidUtils.dp(context, 5F).toFloat()
+    private var fontSize = AndroidUtils.dp(context, 40F).toFloat()
+    private var colors = emptyList<Int>()
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.StatsView) {
+            lineWidth = getDimension(R.styleable.StatsView_lineWidth, lineWidth)
+            fontSize = getDimension(R.styleable.StatsView_fontSize, fontSize)
+            colors = listOf(
+                getColor(
+                    R.styleable.StatsView_color1,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color2,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color3,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color4,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color5,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color6,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color7,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color8,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color9,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color10,
+                    randomColor()
+                )
+            )
+        }
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = lineWidth
+        strokeCap = Paint.Cap.ROUND
+        strokeJoin = Paint.Join.ROUND
+    }
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = fontSize
+    }
+
+    var data: List<Float> = emptyList()
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - lineWidth / 2
+        center = PointF(w / 2F, h / 2F)
+        oval = RectF(
+            center.x - radius, center.y - radius,
+            center.x + radius, center.y + radius,
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (data.isEmpty()) {
+            return
+        }
+
+        val dataPercent = arrayListOf<Float>()
+
+        for ((index, element) in data.withIndex()) {
+            dataPercent.add(index, element / data.sum())
+        }
+
+        var startFrom = -90F
+        val firstColor = colors.getOrNull(0) ?: randomColor()
+        startFrom += drawData(dataPercent[0], 0, canvas, startFrom, firstColor)
+
+        for (i in 1..dataPercent.lastIndex) {
+            startFrom += drawData(dataPercent[i], i, canvas, startFrom)
+        }
+
+        paint.color = firstColor
+        canvas.drawPoint(center.x, oval.top, paint)
+
+        canvas.drawText(
+            "%.2f%%".format(dataPercent.sum() / dataPercent.average() * 100 / NUMBER_OF_DIVISIONS),
+            center.x,
+            center.y + textPaint.textSize / 4,
+            textPaint,
+        )
+    }
+    private fun drawData(
+        datum: Float,
+        index: Int,
+        canvas: Canvas,
+        startFrom: Float,
+        color: Int = 0
+    ): Float {
+        val angle = 360F * datum * data.size / NUMBER_OF_DIVISIONS
+        paint.color = if (color == 0) colors.getOrNull(index) ?: randomColor() else color
+        canvas.drawArc(oval, startFrom, angle, false, paint)
+        return angle
+    }
+
+    private fun randomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+}
